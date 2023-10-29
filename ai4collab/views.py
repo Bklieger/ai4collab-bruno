1b0af159889570ad0edb66f026eed853d48f501d
Ben Klieger <bklieger1@gmail.com>
2023-10-28 21:38:48 -0700

    Fix deployment status formatting

--- ai4collab/views.py
+++ ai4collab/views.py
@@ -14,10 +14,26 @@ from django.http import Http404
 # Import settings, Warning: contains sensitive variables
 from django.conf import settings
 
+# Import Regular Expressions
+import re
 
 #------- [Environment Variables] -------#
 running_deployment_transcript = settings.DEPLOYMENT_TRANSCRIPT # String
 critical_warnings_exist = settings.CRIT_WARNINGS_EXIST # Boolean
+
+
+#------- [Initialization] -------#
+# Replace \x1b\[93m with newline
+running_deployment_transcript = re.sub(r'\x1b\[93m', '\n', running_deployment_transcript)
+
+# Replace \x1b\[92m with newline
+running_deployment_transcript = re.sub(r'\x1b\[92m', '\n', running_deployment_transcript)
+
+# Replace \x1b\[91m with newline
+running_deployment_transcript = re.sub(r'\x1b\[91m', '\n', running_deployment_transcript)
+
+# Replace \x1b\[91m with newline
+running_deployment_transcript = re.sub(r'\x1b\[0m', '\n', running_deployment_transcript)
 
 
 #------- [Define Views] -------#
@@ -27,6 +43,8 @@ def deployment_status(request):
     if not request.user.is_superuser:
         raise Http404 # If not superadmin, raise 404 error
     
-    # Render deployment status page and pass in environment variables
-    return render(request, 'deployment_status.html', {'running_deployment_transcript': running_deployment_transcript, 'critical_warnings_exist': critical_warnings_exist})
+    # Turn into array seperated by newlines
+    transcript = running_deployment_transcript.split('\n')
 
+    # Render deployment status page and pass in environment variables
+    return render(request, 'deployment_status.html', {'running_deployment_transcript': transcript, 'critical_warnings_exist': critical_warnings_exist})
