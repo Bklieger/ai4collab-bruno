6574133daf8aab4e263550db37e4acec559356c5
Ben Klieger <bklieger1@gmail.com>
2024-01-11 20:37:47 -0800

    Add LLM only API endpoint
    Add llm_only endpoint where transcript is provided by user. For upload page.

--- llm/urls.py
+++ llm/urls.py
@@ -10,8 +10,9 @@ Date: 2024-01-07
 from django.urls import path
 
 # Import view of LLM
-from .views import LlmView
+from .views import LlmView, LlmOnlyView
 
 urlpatterns = [
     path('api/llm/', LlmView.as_view(), name='llm'), # LLM view
+    path('api/llm_only/', LlmOnlyView.as_view(), name='llm_only'), # LLM view
 ]
